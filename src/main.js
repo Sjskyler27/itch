1ef33053d6acab87a32525d6365342488ec5472a
Skyler Simpson <84102384+Sjskyler27@users.noreply.github.com>
2023-10-20 17:51:28 -0600

    installed packeges

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// create the app
+import { createApp } from 'vue';
+import App from './App.vue';
+const app = createApp(App);
 
-createApp(App).mount('#app')
+// get the created router
+import router from './router';
+app.use(router);
+
+// Add Global components
+import BaseCard from './components/UI/BaseCard.vue';
+import TheHeader from './components/layouts/TheHeader';
+import BaseButton from './components/UI/BaseButton';
+import BaseUl from './components/UI/BaseUl';
+import FaqCard from './components/UI/FaqCard';
+app.component('BaseUl', BaseUl);
+app.component('BaseButton', BaseButton);
+app.component('BaseCard', BaseCard);
+app.component('TheHeader', TheHeader);
+app.component('FaqCard', FaqCard);
+
+app.mount('#app');
