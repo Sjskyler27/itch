d7dbbb2a67de23a46f9057bc1cd7f13657f041ba
Skyler Simpson <84102384+Sjskyler27@users.noreply.github.com>
2023-10-20 18:08:30 -0600

    assets loaded

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,124 @@
+<template>
+  <header class="stonecrest-header">
+    <a href="/" class="logo-title">
+      <img
+        src="../../../public/itchbadgepng.png"
+        alt="Stonecrest Suites Logo"
+        class="logo"
+      />
+      <h1>Itch</h1>
+    </a>
+    <div class="menu-container" @click="toggleMenu">
+      <!-- <DarkToggle></DarkToggle> -->
+      <div class="menu-icon"><HamburgerButton></HamburgerButton></div>
+      <transition name="slide-fade">
+        <div v-if="menuVisible" class="dropdown-menu">
+          <router-link to="/settings">Settings</router-link>
+          <router-link to="/profile">Profile</router-link>
+          <router-link to="/account">Account</router-link>
+          <router-link to="/support">Support</router-link>
+          <router-link to="/about">About Us</router-link>
+          <router-link to="/logout">Logout</router-link>
+        </div>
+      </transition>
+    </div>
+  </header>
+</template>
+
+<script>
+import HamburgerButton from '../UI/HamburgerButton.vue';
+// import DarkToggle from '../UI/DarkToggle.vue';
+export default {
+  components: {
+    HamburgerButton,
+    // DarkToggle,
+  },
+  name: 'StonecrestHeader',
+  data() {
+    return {
+      menuVisible: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.menuVisible = !this.menuVisible;
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@import '@/assets/main.scss';
+.stonecrest-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 20px;
+  background-color: #fff;
+}
+
+.logo-title {
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  color: $primary-color;
+  font-family: Inika;
+}
+
+.logo {
+  height: 70px;
+  margin-right: 10px;
+}
+.menu-container {
+  position: relative;
+}
+
+.menu-icon {
+  font-size: 24px;
+  padding: 7px;
+  cursor: pointer;
+  position: relative;
+}
+
+.dropdown-menu {
+  position: absolute;
+  top: 100%;
+  right: 0;
+  background: linear-gradient(
+    to top right,
+    lighten($primary-color, 50%),
+    #e9e9ea
+  );
+  border: 1px solid lighten($primary-color, 50%);
+  box-shadow: 0 2px 8px lighten($primary-color, 50%);
+  border-radius: 5px;
+  color: white;
+}
+
+.dropdown-menu a {
+  display: block;
+  padding: 8px 12px;
+  text-decoration: none;
+  color: black;
+}
+
+.dropdown-menu a:hover,
+a.router-link-active {
+  color: #f5f5f5;
+}
+/* Animation styles */
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: opacity 0.3s ease, transform 0.3s ease;
+}
+
+.slide-fade-enter-from, .slide-fade-leave-to /* <- This is the alias for .slide-fade-leave-active in Vue 2.6+ */ {
+  opacity: 0;
+  transform: translateY(-10px);
+}
+
+.slide-fade-enter-to, .slide-fade-leave-from /* <- This is the alias for .slide-fade-leave in Vue 2.6+ */ {
+  opacity: 1;
+  transform: translateY(0);
+}
+</style>
---
+++ src/pages/ExamplePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <BaseButton>Click me</BaseButton>
+    <BaseButton class="flat">Click me</BaseButton>
+  </div>
+  <div class="faq-container">
+    <FaqCard class="faq">
+      <template v-slot:header> Headline </template>
+      This is a placeholder text for sampling the content of the label I am
+      making.
+    </FaqCard>
+  </div>
+</template>
